b80ad253378988ff33115a5e78d15a8354ce676a
vandannandwana-affordmed <vandannandwana@affordmed.com>
2026-01-21 09:52:54 +0530

    added express app using typescript

---
+++ express/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
